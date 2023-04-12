ad1840188206b338dacfb079aa240c487aaec876
RenanTeixeiras <95451167+RenanTeixeiras@users.noreply.github.com>
2023-04-11 21:28:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
+from datetime import date
 
 app = Flask(__name__)
 #ENDEREÇO DO BANCO
@@ -21,7 +22,6 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 def current_user(user_id):
     return Usuario.query.get(user_id)
 
-
 class Usuario(db.Model, UserMixin):
     __tablename__ = "usuario"
     id = db.Column(db.Integer, primary_key=True)
@@ -31,6 +31,19 @@ class Usuario(db.Model, UserMixin):
 
     def __str__(self):
         return self.nome
+    
+class Tarefas(db.Model):
+    __tablename__ = "tarefas"
+    id_tarefa = db.Column(db.Integer, primary_key=True)
+    titulo_tarefa = db.Column(db.String(84), nullable=False)
+    hora_tarefa = db.Column(db.String(84), nullable=False)
+    prazo_tarefa = db.Column(db.String(84), nullable=False)
+    prioridade = db.Column(db.String(84), nullable=False)
+    
+    def __str__(self):
+        return self.titulo_tarefa
+    
+
 db.create_all()    
 
 
@@ -46,7 +59,7 @@ def cadastro():
         db.session.commit()
     return render_template('cadastro.html')
 
-@app.route("/login", methods=['GET','POST'])
+@app.route("/", methods=['GET','POST'])
 def login():
     if request.method == "POST":
         email= request.form['email']
@@ -54,24 +67,49 @@ def login():
         usuario = Usuario.query.filter_by(email=email).first()
     
         if not usuario:
+            flash('Usuário não encontrado!')
             return redirect(url_for("login"))
         
         if not check_password_hash(usuario.senha, senha):
+            flash('Senha incorreta!')
             return redirect(url_for('login'))
         
         login_user(usuario)
-        return redirect(url_for("index"))
+        return redirect(url_for("tarefas"))
     return render_template('login.html')   
 
-@app.route("/")
+@app.route("/usuarios")
 @login_required
-def index():
+def usuarios():
     usuarios = Usuario.query.all()
-    return render_template('index.html', usuarios=usuarios)  
+    return render_template('usuarios.html', usuarios=usuarios)  
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route("/tarefas", methods=['GET', 'POST'])
+@login_required
+def tarefas():
+    if request.method == "POST":
+        tarefa= Tarefas()
+        tarefa.titulo_tarefa = request.form['tarefa']
+        tarefa.hora_tarefa = (date.today()).strftime("%d/%m/%Y")
+        tarefa.prazo_tarefa = request.form['prazo']
+        tarefa.prioridade = request.form['prioridade']
+        db.session.add(tarefa)
+        db.session.commit()
+        return redirect(url_for('tarefas'))
+    tarefas = Tarefas.query.all()
+    return render_template('tarefas.html', tarefas=tarefas)
+
+@app.route("/deletar/<int:id>")
+@login_required
+def deletar_tarefa(id):
+    tarefa = Tarefas.query.filter_by(id_tarefa=id).first()
+    db.session.delete(tarefa)
+    db.session.commit()
+    return redirect(url_for('tarefas'))
 
 if __name__ == "__main__":
     app.run(debug=True)
